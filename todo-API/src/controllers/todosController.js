2aefc62c4e1973b4811252e38ec53bdd13f18b2f
Blade254 <lightblade254@gmail.com>
2025-06-23 12:36:41 +0300

    added createTodo route

--- todo-API/src/controllers/todosController.js
+++ todo-API/src/controllers/todosController.js
@@ -1,4 +1,4 @@
-const { pool, poolConnect } = require('../db/dbConfig');
+const { pool, poolConnect, mssql } = require('../db/dbConfig');
 
 //get all todods
 const getAllTodos = async (req, res) => {
@@ -19,6 +19,36 @@ const getAllTodos = async (req, res) => {
     }
 };
 
+//create todo
+const createTodo = async (req, res) => {
+    try{
+        const { task_text } = req.body;
+
+        //checking if task_text is provided
+        if(!task_text) {
+            return res.status(400).json({ message: 'Task text cannot be empty' });
+        }
+
+        await poolConnect;
+        const request = pool.request();
+
+        //parameterized queries to prevent SQL injection
+        request.input('taskText', mssql.NVarChar, task_text);
+
+        //insert query
+        const result = await request.query(
+            'INSERT INTO todos (task_text) OUTPUT INSERTED.* VALUES (@taskText)'
+        );
+
+        //send the newly created todo as a response
+        res.status(201).json(result.recordset[0]);
+    } catch (err) {
+        console.error('Error creating todo:', err);
+        res.status(500).send('Server error');
+    }
+} 
+
 module.exports ={
     getAllTodos,
+    createTodo
 };
--- todo-API/src/routes/todoRoutes.js
+++ todo-API/src/routes/todoRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router= express.Router();
 
 
-const {  getAllTodos } = require('../controllers/todosController');
+const {  getAllTodos,createTodo } = require('../controllers/todosController');
 
 // Define the route to get all todos
 router.get('/', getAllTodos);
+
+//create todo route
+router.post('/', createTodo);
 
 // Export the router
 module.exports = router;
